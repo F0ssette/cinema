7334febaa031e4553946257fd0c89f7f15f83a2d
Lucie KOZIK <lucie.kozik@hotmail.fr>
2020-03-16 17:15:03 +0100

    Mise en place d'Angular pour faire la partie front de mon dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { ListComponent as Actor} from './Actor/list/list.component';
+import { ListComponent as Movie} from './Movie/list/list.component';
+import { ListComponent as Gender} from './Gender/list/list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'actor', component: Actor},
+  {path:'movie', component: Movie},
+  {path:'gender', component: Gender},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './Movie/list/list.component';
+import { AddComponent } from './Movie/add/add.component';
+import { EditComponent } from './Movie/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
